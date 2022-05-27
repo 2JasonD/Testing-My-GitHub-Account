838f18206a07fa7d07d9bd322031f1fd0269ce4e
2JasonD <98455401+2JasonD@users.noreply.github.com>
2022-05-28 01:56:14 +0200

    Add project files.

---
+++ Login Form/Controllers/Login.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Login_Form.Models;
+
+namespace Login_Form.Controllers
+{
+    public class Login : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+
+            //Here w set up an if statement to verify is the logins used
+            // are the correct ones
+
+        {
+            if(userModel.Username =="Jason" && userModel.Password =="jas123")
+            {
+                return View("LoginSuccess", userModel);
+                // if that did not work, we use the else statement
+                // and return another view called Loginfailure, saying to the user that
+                // did not work that
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+
+    }
+}
---
+++ Login Form/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Login_Form.Models
+{
+    public class UserModel
+    {
+        public string ID { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
